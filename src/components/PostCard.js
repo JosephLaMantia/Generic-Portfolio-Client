2e688d4f08acd56b4d983c733c4a2a230f1e0bba
Joseph LaMantia Jr <40052124+JosephLaMantia@users.noreply.github.com>
2021-03-17 17:20:34 -0400

    Edited link colors.

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import {Card} from 'react-bootstrap'
+import React from 'react';
+import {Card} from 'react-bootstrap';
+import { Instagram } from 'react-feather';
 
 export default ({title, url, date, link, description}) => {
 
     return (
         <div className="PostCard">
             <Card style={{ width: '100%', borderWidth: '0px' }}>
-            <Card.Img variant="top" src={url}/>
+            <a href={link}><Card.Img variant="top" src={url}/></a>
             <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>{description}</Card.Text>
-                <Card.Text>{date}</Card.Text>
+                <Card.Title style={{textAlign: 'center'}}>{title}</Card.Title>
+                <Card.Text style={{textAlign: 'center', paddingBottom: '3vh'}}>{description}</Card.Text>
+                <Card.Text style={{fontSize: '1.4vh'}}><a href={link}><Instagram size={20}/></a> {date}</Card.Text>
             </Card.Body>
             </Card>
         </div>
